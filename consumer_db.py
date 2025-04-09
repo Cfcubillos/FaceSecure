dae3cd5c2bdb672bdc7faa75e0554c180b0432e1
Felipe Cubillos <felipecubillos@MacBook-Pro-de-Felipe-Cubillos.local>
2025-04-09 16:34:53 -0500

    Importar la librerias para el consumidor

---
+++ consumer_db.py
@@ -0,0 +1,5 @@
+import pika  # Cliente RabbitMQ
+import json  # Para procesar mensajes
+import base64  # Decodificar imágenes
+import mysql.connector  # Conector MySQL
+from mysql.connector import Error  # Manejo de errores
