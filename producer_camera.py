4f31a676ec7351aeea4e952b5d5733a99be3e7d3
Felipe Cubillos <felipecubillos@MacBook-Pro-de-Felipe-Cubillos.local>
2025-04-09 16:29:24 -0500

    Importar la librerias necesarias para el proyecto

---
+++ producer_camera.py
@@ -0,0 +1,5 @@
+import cv2  # Librería para captura y procesamiento de imágenes. 
+import pika  # Cliente para interactuar con RabbitMQ.
+import base64  # Para codificar imágenes a base64.
+import json  # Para convertir mensajes a formato JSON.
+import time  # Para obtener timestamp.
